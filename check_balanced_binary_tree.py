7b2194b4d5e8fa53ea5eed876cac104638a2ec4e
Rohit Gottiparthy <rohitgottiparthy@gmail.com>
2020-01-09 09:37:48 -0800

    Create check_balanced_binary_tree.py

---
+++ check_balanced_binary_tree.py
@@ -0,0 +1,22 @@
+# LC 110
+# A binary tree is said to be balanced, if the difference in height of its left and right subtrees is at most 1
+"""
+Ex:          12       Height: left subtree is 2, right is 1. so its balanced. If left has one more node, its unbalanced.
+            /  \
+          14    22
+          /\    
+         15 19 
+         
+This can be represented as a list - [12,14,22,15,19]         
+"""
+         
+         
+         
+def test_balance(tree):
+    
+    i
+    
+    if height > 1:
+        return False
+    return True
+    
