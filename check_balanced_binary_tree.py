d6eb1461cbc1a445deae0126523905916d431c9d
Rohit Gottiparthy <rohitgottiparthy@gmail.com>
2020-06-24 11:18:49 -0700

    Update check_balanced_binary_tree.py

--- check_balanced_binary_tree.py
+++ check_balanced_binary_tree.py
@@ -11,12 +11,3 @@ This can be represented as a list - [12,14,22,15,19]
 """
          
          
-         
-def test_balance(tree):
-    
-    i
-    
-    if height == 0 or height == 1:
-        return True
-    return False
-    
